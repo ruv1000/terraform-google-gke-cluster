ba6a97bca0d215dffb3cd3215314e0c22bcaa31a
Ihor.shevchenko <magicruv@gmail.com>
2024-01-08 16:26:29 +0200

    Add modules

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-# provider "google" {
-#   # Configuration options
-#   project = var.GOOGLE_PROJECT
-#   region  = var.GOOGLE_REGION
-# }
+provider "google" {
+  # Configuration options
+  project = var.GOOGLE_PROJECT
+  region  = var.GOOGLE_REGION
+}
 
-# module "gke_cluster" {
-#   source         = "github.com/ruv1000/tf-google-gke-cluster"
-#   GOOGLE_REGION  = var.GOOGLE_REGION
-#   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-#   GKE_NUM_NODES  = 2
-# }
+module "gke_cluster" {
+  source         = "github.com/ruv1000/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 2
+}
 
 module "github_repository" {
   source = "github.com/ruv1000/tf-github-repository"
@@ -28,16 +28,37 @@ module "tls_private_key" {
   ecdsa_curve = var.ecdsa_curve
 }
 
-module "tf-kind-cluster" {
-  source = "github.com/ruv1000/tf-kind-cluster"
-
-}
+# module "tf-kind-cluster" {
+#   source = "github.com/ruv1000/tf-kind-cluster"
+# }
 
 module "flux_bootstrap" {
   source = "github.com/ruv1000/tf-fluxcd-flux-bootstrap"
 
   github_repository = "${var.github_owner}/${var.github_repository}"
   private_key       = module.tls_private_key.private_key_pem
-  # config_path       = module.gke_cluster.kubeconfig
-  github_token = var.github_token
+  config_path       = "~/.kube/config" # module.gke_cluster.kubeconfig
+  github_token      = var.github_token
+}
+
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
+
+module "kms" {
+  source          = "github.com/ruv1000/terraform-google-kms"
+  project_id      = var.GOOGLE_PROJECT
+  keyring         = "sops-flux"
+  location        = "global"
+  keys            = ["sops-key-flux"]
+  prevent_destroy = false
+
 }
--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 # ${path.module} is an interpolated string in Terraform, which references the path to the current
 # module. In this case, it returns the path to the directory containing the current module.
 
-output "kubeconfig" {
-  value       = module.tf-kind-cluster.kubeconfig
-  description = "The path to the kubeconfig file"
-}
+# output "kubeconfig" {
+#   value       = module.tf-kind-cluster.kubeconfig
+#   description = "The path to the kubeconfig file"
+# }
 
 output "private_key_pem" {
   value     = module.tls_private_key.private_key_pem
